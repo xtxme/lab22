c6dd39719ddb95e42293ab2bab568cc465b7e96f
xtxme <tt.icy013gmail.com>
2024-03-01 00:17:20 +0700

    Lab 22 average complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc,char *argv []){
+    
+    double sum;
+    if (argc==1)
+    {
+        cout<< "Please input numbers to find average.";
+    } else {
+        cout<< "---------------------------------\n";
+        for (int i = 1; i < argc; i++)
+        {
+            double num = atof(argv[i]);
+            sum += num;
+        }
+        double average = sum/(argc-1);
+        cout << "Average of " << argc-1 << " numbers = " << average;
+        cout<< "\n---------------------------------";
+    }
+
+    return 0;
+
+}
